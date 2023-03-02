55e059560f60d69225733e8c570e001263678112
Alexandr Galayura <first6886@mail.ru>
2023-03-02 19:03:03 +0300

    add db.

--- db/db_VKtinder.py
+++ db/db_VKtinder.py
@@ -1,7 +1,10 @@
 from db_VKtinder_models import User, Candidates, Favorites
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
-
-DSN = f'postgresql://{os.getenv("LOGIN")}:{os.getenv("PASSWORD")}@{os.getenv("SERVER")}:{os.getenv("PORT")}/{os.getenv("DB_NAME")}'
+DSN = f'postgresql://{os.getenv("LOGIN")}:{os.getenv("PASSWORD")}@' \
+      f'{os.getenv("SERVER")}:{os.getenv("PORT")}/{os.getenv("DB_NAME")}'
 engine = sqlalchemy.create_engine(DSN)
 
 Session = sessionmaker(bind=engine)
