295f68bd7e64404b61260d3f2ea89b410aaf0d9a
Vladislava Mayorova <vladamayor@yandex.ru>
2023-03-02 16:10:11 +0300

    change bot.py

---
+++ vk_bot/bot.py
@@ -0,0 +1,63 @@
+import vk_api
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from bot_function import write_msg, collect_data
+
+vk_session = vk_api.VkApi(token=os.getenv('access_token_community'))
+longpoll = VkLongPoll(vk_session)
+
+
+answer = ['привет', 'старт']
+
+dict_data = {}
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        user_id = event.user_id
+        text = event.text.lower()
+    
+        if text in answer or text.split()[1] == 'жен' or 'муж':
+
+            if text == 'привет':
+                keyboard = VkKeyboard(one_time=True)
+                keyboard.add_button('Старт', VkKeyboardColor.POSITIVE)
+                write_msg(vk_session, user_id, f'Приветик! Показать что я умею? Жми на "Старт"', keyboard)
+
+            elif text == 'старт':
+                write_msg(vk_session, user_id, 'Могу показать тех, кто тебе подходит. \n\
+                        Напиши через пробел: \n 1. Возраст \n 2. Пол (муж/жен) \n 3. Город', keyboard)
+
+
+            elif text.split()[1] == 'жен' or 'муж':
+                data = text.split()
+                collect_data(data, user_id, dict_data)
+
+                keyboard = VkKeyboard(one_time=True)
+                keyboard.add_button('Поехали!', VkKeyboardColor.POSITIVE)
+                write_msg(vk_session, user_id, 'Отлично! Уже ищу подходящих собеседников! Если готов, жми "Поехали!"', keyboard)
+                break
+
+        else:
+            write_msg(vk_session, user_id, 'По-инопланетному пока не умею.. но я учусь')
+
+print(dict_data)
+
+  
+
+
+
+
+
+            
+            
+# elif text == 'выход':
+#     write_msg(user_id, 'Пока, приходи еще!')
+#     break
+
+# buttons = ['Поехали!', 'Выход']
+# buttons_color = [VkKeyboardColor.POSITIVE, VkKeyboardColor.NEGATIVE] 
+# for btn, btn_color in zip(buttons, buttons_color):
+#     keyboard.add_button(btn, btn_color)
