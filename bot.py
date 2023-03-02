06136e43d0dcb178744310701ba9c820a46cff77
Vladislava Mayorova <vladamayor@yandex.ru>
2023-03-02 14:13:27 +0300

    change bot.py

--- bot.py
+++ bot.py
@@ -5,43 +5,67 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 vk_session = vk_api.VkApi(token=os.getenv('access_token_community'))
 
 longpoll = VkLongPoll(vk_session)
 
-def write_msg(user_id, message):
-    vk_session.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+def write_msg(user_id, message, keyboard=None):
+    post = {'user_id': user_id, 
+            'message': message,
+            'random_id': randrange(10 ** 7)
+    }
+    if keyboard != None:
+        post['keyboard'] = keyboard.get_keyboard()
+    else:
+        post = post
 
-list_data = []
+    vk_session.method('messages.send', post)
 
 
-while True:
-    try:
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-                user_id = event.user_id
-                text = event.text.lower()
 
-                if text == 'привет' or text == 'здравствуйте' or text == 'здравствуй':
-                    write_msg(user_id, f'Приветик! Показать что я умею?')
+dict_data = {}
 
-                elif text == 'да' or text == 'давай':
-                    write_msg(user_id, 'Могу показать тех, кто тебе подходит. \n\
-                            Напиши через пробел: \n 1. Возраст \n 2. Пол (муж/жен) \n 3. Город')
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        user_id = event.user_id
+        text = event.text.lower()
 
-                elif text.split()[1] == 'жен' or 'муж':
-                    data = text.split()
-                    write_msg(user_id, 'Отлично! Уже ищу подходящих собеседников!')
-                    list_data.append(data)
-                    
-                elif text == 'пока' or text == 'нет':
-                    write_msg(user_id, f'До скорого, приходи еще!')
+        if text == 'привет' or text == 'здравствуйте' or text == 'здравствуй':
+            keyboard = VkKeyboard(one_time=True)
+            keyboard.add_button('Старт', VkKeyboardColor.POSITIVE)
+            write_msg(user_id, f'Приветик! Показать что я умею? Жми на "Старт"', keyboard)
 
-                else:
-                    write_msg(user_id, 'Не понял, повторите еще раз, пожалуйста')
-    except:
-        pass
+        elif text == 'старт':
+            write_msg(user_id, 'Могу показать тех, кто тебе подходит. \n\
+                    Напиши через пробел: \n 1. Возраст \n 2. Пол (муж/жен) \n 3. Город', keyboard)
 
-        
-print(list_data)
+
+        elif text.split()[1] == 'жен' or 'муж':
+            data = text.split()
+            data[2].title()
+            dict_data['id'] = user_id
+            dict_data['params'] = data
+
+            keyboard = VkKeyboard(one_time=True)
+            keyboard.add_button('Поехали!', VkKeyboardColor.POSITIVE)
+            write_msg(user_id, 'Отлично! Уже ищу подходящих собеседников! Если готов, жми "Поехали!"', keyboard)
+            break
+
+print(dict_data)
+
+dict_data
+            
+
+
+            
+            
+# elif text == 'выход':
+#     write_msg(user_id, 'Пока, приходи еще!')
+#     break
+
+# buttons = ['Поехали!', 'Выход']
+# buttons_color = [VkKeyboardColor.POSITIVE, VkKeyboardColor.NEGATIVE] 
+# for btn, btn_color in zip(buttons, buttons_color):
+#     keyboard.add_button(btn, btn_color)
