4e5de86c41b264a8d004eb8547be1f8a9948a788
Alexandr Galayura <first6886@mail.ru>
2023-03-03 15:19:34 +0300

    db

--- db/db_VKtinder.py
+++ db/db_VKtinder.py
@@ -3,8 +3,8 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
 
+load_dotenv(find_dotenv())
 
 DSN = f'postgresql://{os.getenv("LOGIN")}:{os.getenv("PASSWORD")}@' \
       f'{os.getenv("SERVER")}:{os.getenv("PORT")}/{os.getenv("DB_NAME")}'
@@ -16,12 +16,44 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-
 # Добавление ID user в базу
 def adding_data_user(data):
-    session.add_all(user_id=data)
+    session.add(User(user_id=data))
     session.commit()
     session.close()
 
 
+test_id = 1111111
+adding_data_user(test_id)
 
+
+# Добавляет даныые candidates в базу.
+# (id кандидата, тья и фамилию, ссылку на страничку кандидата, фото, id user)
+def adding_data_candidates(candidate_id, first_last_name, link, photos, user):
+    session.add(User(candidate_id=candidate_id, first_last_name=first_last_name,
+                     link=link, photos=photos, user=user))
+    session.commit()
+    session.close()
+
+
+test_can_id = 7849365
+test_f_l_name = 'Майская Даздраперма'
+test_link = 'https://vk.com/id7849365'
+test_photos = 'https://sun1-85.userapi.com/impg/qJXqmpjIewHkB2_TtP8lKTmnh572_hkDoLCXgA/uhXitPz3oEg.jpg?size=750x926&quality=95&sign=2b22adf8e9901fd75f445123917f15ed&c_uniq_tag=clixN-h283e4NKP6-Ly57wLFkrEqefw9ssC176J829c&type=album, ' \
+              'https://sun9-54.userapi.com/impg/YVOykrl63QfNyG20FUEH7WPSFXwYGbUpFIP22A/tF1WwSX_VgI.jpg?size=1280x1273&quality=95&sign=6bae4972ec0097cb7422f38e6c348c3a&c_uniq_tag=_G3Rkht8mtolXwt4q-H6LbXG0AfoFdyi12n7vXr--2k&type=album, ' \
+              'https://sun1-47.userapi.com/impg/OYSCmxU7SVrrgunYwsEG0HSk23jQcc_pYWfijg/B7plNi883rw.jpg?size=886x1080&quality=96&sign=31e3e06cd433a6caff446c630439025e&c_uniq_tag=q2sR8eLMMRhMs1suMmLl2I1fHO7o0DGdK2p4qgpKDLk&type=album'
+test_user = 1
+adding_data_candidates(test_can_id, test_f_l_name, test_link, test_photos, test_user)
+
+
+# Добавляет даныые в базу favorites.
+# (id кандидата, тья и фамилию, ссылку на страничку кандидата, фото, id user)
+def adding_data_favorites(favorite_id, first_last_name, link, photos, user):
+    session.add(User(favorite_id=favorite_id, first_last_name=first_last_name,
+                     link=link, photos=photos, user=user))
+    session.commit()
+    session.close()
+
+
+test = session.query(Candidat).all()
+print(test)
--- db/db_VKtinder_models.py
+++ db/db_VKtinder_models.py
@@ -1,40 +1,42 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 
-
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
     id = sq.Column(sq.Integer, primary_key=True)
     user_id = sq.Column(sq.Integer, unique=True)
 
+    def __str__(self):
+        return f'{self.id}, {self.user_id}'
 
 
 class Candidat(Base):
     __tablename__ = "candidates"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    user_id = sq.Column(sq.Integer, unique=True)
-    first_name = sq.Column(sq.String(length=40))
-    last_name = sq.Column(sq.String(length=40))
+    candidate_id = sq.Column(sq.Integer, unique=True)
+    first_last_name = sq.Column(sq.String(length=40))
     link = sq.Column(sq.String(length=1000))
     photos = sq.Column(sq.String(length=1000))
     id_user = sq.Column(sq.Integer, sq.ForeignKey("users.id"), nullable=False)
 
     user = relationship(User, backref="candidat")
 
+
 class Favorit(Base):
     __tablename__ = "favorites"
     id = sq.Column(sq.Integer, primary_key=True)
-    user_id = sq.Column(sq.Integer, unique=True)
-    first_name = sq.Column(sq.String(length=40))
-    last_name = sq.Column(sq.String(length=40))
+    favorite_id = sq.Column(sq.Integer, unique=True)
+    first_last_name = sq.Column(sq.String(length=40))
     link = sq.Column(sq.String(length=1000))
     photos = sq.Column(sq.String(length=1000))
     id_user = sq.Column(sq.Integer, sq.ForeignKey("users.id"), nullable=False)
 
     user = relationship(User, backref="favorit")
+
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
