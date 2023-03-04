cc483cfc665d2a3de4d52d76cdc67271ea1974f5
Alexandr Galayura <first6886@mail.ru>
2023-03-04 22:59:49 +0300

    db fun new

--- db/db_VKtinder.py
+++ db/db_VKtinder.py
@@ -19,18 +19,9 @@ session = Session()
 
 # Добавление ID user в базу
 def adding_data_user(data):
-    session.add(User(user_id=data))
+    session.add(User(user_vk_id=data))
     session.commit()
     session.close()
-
-
-test_id = 1111111
-# adding_data_user(test_id)
-
-test_id1 = 2222222
-
-
-# adding_data_user(test_id1)
 
 
 # Добавляет даныые candidates в базу.
@@ -42,38 +33,19 @@ def adding_data_candidates(candidate_id, first_last_name, link, photos, user):
     session.close()
 
 
-test_can_id = 7849365
-test_f_l_name = 'Майская Даздраперма'
-test_link = 'https://vk.com/id7849365'
-test_photos = 'https://sun1-85.userapi.com/impg/qJXqmpjIewHkB2_TtP8lKTmnh572_hkDoLCXgA/uhXitPz3oEg.jpg?size=750x926&quality=95&sign=2b22adf8e9901fd75f445123917f15ed&c_uniq_tag=clixN-h283e4NKP6-Ly57wLFkrEqefw9ssC176J829c&type=album, ' \
-              'https://sun9-54.userapi.com/impg/YVOykrl63QfNyG20FUEH7WPSFXwYGbUpFIP22A/tF1WwSX_VgI.jpg?size=1280x1273&quality=95&sign=6bae4972ec0097cb7422f38e6c348c3a&c_uniq_tag=_G3Rkht8mtolXwt4q-H6LbXG0AfoFdyi12n7vXr--2k&type=album, ' \
-              'https://sun1-47.userapi.com/impg/OYSCmxU7SVrrgunYwsEG0HSk23jQcc_pYWfijg/B7plNi883rw.jpg?size=886x1080&quality=96&sign=31e3e06cd433a6caff446c630439025e&c_uniq_tag=q2sR8eLMMRhMs1suMmLl2I1fHO7o0DGdK2p4qgpKDLk&type=album'
-test_user = 1
-# adding_data_candidates(test_can_id, test_f_l_name, test_link, test_photos, test_user)
-
-test_can_id1 = 7849366
-test_f_l_name1 = 'Полудурок Иван'
-test_link1 = 'https://vk.com/id7849366'
-test_photos1 = 'https://sun1-85.userapi.com/impg/qJXqmpjIewHkB2_TtP8lKTmnh572_hkDoLCXgA/uhXitPz3oEg.jpg?size=750x926&quality=95&sign=2b22adf8e9901fd75f445123917f15ed&c_uniq_tag=clixN-h283e4NKP6-Ly57wLFkrEqefw9ssC176J829c&type=album, ' \
-               'https://sun9-54.userapi.com/impg/YVOykrl63QfNyG20FUEH7WPSFXwYGbUpFIP22A/tF1WwSX_VgI.jpg?size=1280x1273&quality=95&sign=6bae4972ec0097cb7422f38e6c348c3a&c_uniq_tag=_G3Rkht8mtolXwt4q-H6LbXG0AfoFdyi12n7vXr--2k&type=album, ' \
-               'https://sun1-47.userapi.com/impg/OYSCmxU7SVrrgunYwsEG0HSk23jQcc_pYWfijg/B7plNi883rw.jpg?size=886x1080&quality=96&sign=31e3e06cd433a6caff446c630439025e&c_uniq_tag=q2sR8eLMMRhMs1suMmLl2I1fHO7o0DGdK2p4qgpKDLk&type=album'
-test_user1 = 1
-
-
-# adding_data_candidates(test_can_id1, test_f_l_name1, test_link1, test_photos1, test_user1)
-
 # Добавляет даныые в базу favorites.
 # (id кандидата, тья и фамилию, ссылку на страничку кандидата, фото, id user)
-# def adding_data_favorites(favorite_id, first_last_name, link, photos, user):
-#     session.add(Favorit(favorite_id=favorite_id, first_last_name=first_last_name,
-#                         link=link, photos=photos, id_user=user))
-#     session.commit()
-#     session.close()
+def adding_data_favorites(favorite_vk_id, first_last_name, link, photos, user):
+    session.add(Favorit(favorite_vk_id=favorite_vk_id, first_last_name=first_last_name,
+                        link=link, photos=photos, id_user=user))
+    session.commit()
+    session.close()
+
 
 # Выдает по одному кандидатов.
 def issues_candidate(user_id):
     # Поиск ID в базе по его id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
-    res = session.query(User.id).filter(User.user_id == user_id).all()
+    res = session.query(User.id).filter(User.user_vk_id == user_id).all()
     ress = res[0][0]
     # Выдает по одному кандидатов.
     for result in session.query(Candidat).filter(Candidat.id_user == ress).all():
@@ -82,11 +54,12 @@ def issues_candidate(user_id):
 
 # Удаление кандидата из таблицы candidates
 def deleted_candidate(candidate_id):
-    session.query(Candidat).filter(Candidat.candidate_id == candidate_id).delete()
+    session.query(Candidat).filter(Candidat.candidate_vk_id == candidate_id).delete()
     session.commit()
     session.close()
 
 
-# print(issues_candidate(test_id))
-for test in session.query(Candidat).all():
-    print(test)
+def deleted_favorite(favorite_id):
+    session.query(Favorit).filter(Favorit.favorite_vk_id == favorite_id).delete()
+    session.commit()
+    session.close()
--- db/db_VKtinder_models.py
+++ db/db_VKtinder_models.py
@@ -7,17 +7,17 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = sq.Column(sq.Integer, primary_key=True)
-    user_id = sq.Column(sq.Integer, unique=True)
+    user_vk_id = sq.Column(sq.Integer, unique=True)
 
     def __str__(self):
-        return f'{self.id}, {self.user_id}'
+        return f'{self.id}, {self.user_vk_id}'
 
 
 class Candidat(Base):
     __tablename__ = "candidates"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    candidate_id = sq.Column(sq.Integer, unique=True)
+    candidate_vk_id = sq.Column(sq.Integer, unique=True)
     first_last_name = sq.Column(sq.String(length=40))
     link = sq.Column(sq.String(length=1000))
     photos = sq.Column(sq.String(length=1000))
@@ -26,19 +26,22 @@ class Candidat(Base):
     user = relationship(User, backref="candidat")
 
     def __str__(self):
-        return f'{self.id}, {self.candidate_id}, {self.first_last_name}, {self.link}, {self.photos}, {self.id_user}'
+        return f'{self.id}, {self.candidate_vk_id}, {self.first_last_name}, {self.link}, {self.photos}, {self.id_user}'
 
 
 class Favorit(Base):
     __tablename__ = "favorites"
     id = sq.Column(sq.Integer, primary_key=True)
-    favorite_id = sq.Column(sq.Integer, unique=True)
+    favorite_vk_id = sq.Column(sq.Integer, unique=True)
     first_last_name = sq.Column(sq.String(length=40))
     link = sq.Column(sq.String(length=1000))
     photos = sq.Column(sq.String(length=1000))
     id_user = sq.Column(sq.Integer, sq.ForeignKey("users.id"), nullable=False)
 
     user = relationship(User, backref="favorit")
+
+    def __str__(self):
+        return f'{self.id}, {self.favorite_vk_id}, {self.first_last_name}, {self.link}, {self.photos}, {self.id_user}'
 
 
 def create_tables(engine):
