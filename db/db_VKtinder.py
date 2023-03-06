bb3081e209e9993fb1bbc858aaf5218eef3787c7
Alexandr Galayura <first6886@mail.ru>
2023-03-06 16:36:32 +0300

    db fun new 1

--- db/db_VKtinder.py
+++ db/db_VKtinder.py
@@ -1,4 +1,4 @@
-from db.db_VKtinder_models import create_tables, User, Candidat, Favorit
+from db_VKtinder_models import create_tables, User, Candidat, Favorit
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 import os
@@ -11,7 +11,7 @@ DSN = f'postgresql://{os.getenv("LOGIN")}:{os.getenv("PASSWORD")}@' \
       f'{os.getenv("SERVER")}:{os.getenv("PORT")}/{os.getenv("DB_NAME")}'
 engine = sqlalchemy.create_engine(DSN)
 
-create_tables(engine)
+# create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -29,34 +29,35 @@ def adding_data_user(data):
         pass
     session.commit()
     session.close()
-    result = session.query(User.user_vk_id).all()
-    print(result)
+
 
 # Добавляет даныые candidates в базу.
-# (id кандидата, тья и фамилию, ссылку на страничку кандидата, фото, id user)
+# (id_vk , имя и фамилию, ссылку на страничку, фото). Параметр user_id принимает vk_id польз-я который сделал запрос.
 def adding_data_candidates(candidate_id, first_last_name, link, photos, user_id):
+    # Поиск ID в базе по id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
     res = session.query(User.id).filter(User.user_vk_id == user_id).all()
     ress = res[0][0]
     session.add(Candidat(candidate_vk_id=candidate_id, first_last_name=first_last_name,
                          link=link, photos=photos, id_user=ress))
     session.commit()
     session.close()
-    result = session.query(Candidat).all()
-    print(result)
 
 
 # Добавляет даныые в базу favorites.
-# (id кандидата, тья и фамилию, ссылку на страничку кандидата, фото, id user)
-def adding_data_favorites(favorite_vk_id, first_last_name, link, photos, user):
+# (id_vk , имя и фамилию, ссылку на страничку, фото). Параметр user_id принимает vk_id польз-я который сделал запрос.
+def adding_data_favorites(favorite_vk_id, first_last_name, link, photos, user_id):
+    # Поиск ID в базе по id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
+    res = session.query(User.id).filter(User.user_vk_id == user_id).all()
+    ress = res[0][0]
     session.add(Favorit(favorite_vk_id=favorite_vk_id, first_last_name=first_last_name,
-                        link=link, photos=photos, id_user=user))
+                        link=link, photos=photos, id_user=ress))
     session.commit()
     session.close()
 
 
 # Выдает по одному кандидатов.
 def issues_candidate(user_id):
-    # Поиск ID в базе по его id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
+    # Поиск ID в базе по id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
     res = session.query(User.id).filter(User.user_vk_id == user_id).all()
     ress = res[0][0]
     # Выдает по одному кандидатов.
@@ -65,7 +66,7 @@ def issues_candidate(user_id):
 
 
 def issues_favorite(user_id):
-    # Поиск ID в базе по его id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
+    # Поиск ID в базе по id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
     res = session.query(User.id).filter(User.user_vk_id == user_id).all()
     ress = res[0][0]
     # Выдает по одному из избранных.
@@ -79,7 +80,25 @@ def deleted_candidate(candidate_id):
     session.close()
 
 
+def deleted_candidate_all(user_id):
+    # Поиск ID в базе по его id в VK. Принимает на вход id VK, на выходе id в базе(Целое число)
+    res = session.query(User.id).filter(User.user_vk_id == user_id).all()
+    ress = res[0][0]
+    # Удалит всех кандидатов для определенного usera
+    session.query(Candidat).filter(Candidat.id_user == ress).delete()
+    session.commit()
+    session.close()
+
+
 def deleted_favorite(favorite_id):
     session.query(Favorit).filter(Favorit.favorite_vk_id == favorite_id).delete()
     session.commit()
     session.close()
+
+
+
+# for result in session.query(Candidat).all():
+#     print(result)
+# for res in session.query(User).all():
+#     print(res)
+
